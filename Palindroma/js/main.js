929b4f28266cea51a9940ff5121ca4792cec505c
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-12 14:18:05 +0200

    build: add base project files and the overall logic

---
+++ Palindroma/js/main.js
@@ -0,0 +1,15 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// prima variabile: chiedo all'utente di inserire una parola
+
+
+// funzione che mi guarda se è palindroma e riporta il risultato:
+
+    // confrontare ogni singola lettere della parola (FOR)
+
+    //se la prima lettera è uguale all'ultima, la seconda è uguale alla penultima e così via -> palindroma (IF)
+    
+    
+    // altrimenti -> non palindroma (ELSE)
---
+++ Pari e Dispari/js/main.js
@@ -0,0 +1,23 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+// prima variabile: chiedo alla persona se scegli pari o dispari con un prompt
+
+
+// seconda variabile: chiedo alla persona di inserire un numero da 1 a 5 col prompt
+
+
+
+// terza variabile: un numero random (prendo la funzione) per il PC
+
+
+
+// quarta variabile: sarà la somma che sarà calcolata con una seconda funzione che mi prenderà il numero della persona e il numero del pc
+
+
+
+// se la somma === prima variabile allora persona vince, altrimenti ha vinto il PC
